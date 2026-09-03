531583bc035b41008c1048ed16a9dc3eda045b2e
Rafay Mirza <mirzaaravi@gmail.com>
2026-09-03 14:27:44 -0400

    fix: deolyment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/rafaymirza-portfolio/',
   plugins: [react(), tailwindcss()],
 })
