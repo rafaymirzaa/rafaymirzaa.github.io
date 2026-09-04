8a18166b5c62eed588a5d1d57fc2541114833e01
Rafay Mirza <mirzaaravi@gmail.com>
2026-09-03 21:58:00 -0400

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rafaymirza-portfolio/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
